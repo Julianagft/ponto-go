7757f39baa30d160ae29ed885b1ce356dbd656de
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-03-07 12:53:10 -0300

    calendario quase pronto

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,6 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     screens: {
-      'sm': '460px',
       'md': '790px'
     }
       
