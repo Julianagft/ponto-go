f2be6e601247d352a78218c1d4c4df7bee7637a5
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-03-07 17:26:46 -0300

    home

---
+++ src/app/homePage/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className='w-full h-full flex flex-col justify-items-center content-center flex-wrap p-8 gap-2.5'>
+            <h1>Seja bem vindo a Ponto Go!</h1>
+            <p>Plataforma para controle de pontos e jornadas personalizadas para sua equipe.</p>
+        </div>
+    )
+}
